c05e6e01ce29fe47b5f39504725e06fa62c2bf19
Jao Padua <paduajao@gmail.com>
2024-05-10 15:23:20 +0800

    reverrt middle

--- Server.js
+++ Server.js
@@ -41,6 +41,15 @@ const corsOptions ={
 app.use(cors(corsOptions));
 
 
+
+//middleware
+app.use((req, res, next)=>{
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+})
+
 //api
 
 app.get("/", (req,res) =>{
