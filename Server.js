13bc08bf80f4c1b907c8f2aded28f2acdaca1996
Jao Padua <paduajao@gmail.com>
2024-05-10 18:14:44 +0800

    corss ulit

--- Server.js
+++ Server.js
@@ -58,12 +58,12 @@ app.use(cors(corsOptions));
 //app.use(cors(corsOptions));
 
 //middleware
-app.use((req, res, next)=>{
-    res.setHeader("Access-Control-Allow-Origin", "*");
+/*app.use((req, res, next)=>{
+    res.setHeader("Access-Control-Allow-Origin", "");
     res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     next();
-})
+})*/
 
 //app.use(cors())
 
