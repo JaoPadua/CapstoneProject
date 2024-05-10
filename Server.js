4fcd3eb9a4e74bf74fb15bf362b06f0f8ccc4495
Jao Padua <paduajao@gmail.com>
2024-05-10 15:37:33 +0800

    remove middleware on server

--- Server.js
+++ Server.js
@@ -43,12 +43,12 @@ app.use(cors(corsOptions));
 
 
 //middleware
-app.use((req, res, next)=>{
+/*app.use((req, res, next)=>{
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     next();
-})
+})*/
 
 //api
 
