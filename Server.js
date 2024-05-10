cee1cf881689237bfa4158ee7135d5a2dd4ee706
Jao Padua <paduajao@gmail.com>
2024-05-10 15:49:25 +0800

    use setheader origin

--- Server.js
+++ Server.js
@@ -57,12 +57,12 @@ const corsOptions = {
 
 
 //middleware
-/*app.use((req, res, next)=>{
+app.use((req, res, next)=>{
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     next();
-})*/
+})
 
 //api
 
