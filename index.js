c52b16d78c60b72ff4be91d64d1168fd82bee142
Jao Padua <paduajao@gmail.com>
2024-05-19 20:47:03 +0800

    adjust body parser for image upload

--- index.js
+++ index.js
@@ -23,13 +23,11 @@ const app = express()
 
 //middleware
 //to make the image Base64 into small size
-
-app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+///app.use(bodyParser.json({ limit: '50mb' }));
+
 app.use(express.json({ limit: '50mb' }));
 
-
-app.use(express.json())
 
 app.use("/uploads",express.static("uploads"))
 
