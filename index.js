741ea59b790813d8601faaf0a50de134ed9dac52
Jao Padua <paduajao@gmail.com>
2024-05-19 20:42:27 +0800

    adjust bodyparser at index

--- index.js
+++ index.js
@@ -22,13 +22,15 @@ const mongoose = require('mongoose')
 const app = express()
 
 //middleware
-
-app.use(express.json())
 //to make the image Base64 into small size
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.json({ limit: '50mb' }));
+
+
+app.use(express.json())
+
 app.use("/uploads",express.static("uploads"))
 
 //Corrs to unblock Access-Control-Allow-Origin old corss
