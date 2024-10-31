cef44414467b5b96ce02c8fd832680b0de5756d8
Xenocrates <72752162+JaoPadua@users.noreply.github.com>
2024-10-31 23:16:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -38,8 +38,7 @@ app.use("/uploads",express.static("uploads"))
 //Corrs to unblock Access-Control-Allow-Origin old corss
 const cors =require('cors')
 const corsOptions = {
-  //origin: 'https://elderlysquire.netlify.app'
-  origin: '*'
+  origin: 'https://elderlysquire.netlify.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
